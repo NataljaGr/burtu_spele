005646533b9dbc795631330d51d7cf849a2a28bf
NataljaGr <90210655+NataljaGr@users.noreply.github.com>
2022-01-21 01:36:05 +0200

    darbs naktī

---
+++ script.js
@@ -0,0 +1,66 @@
+let Vardi =["SKOLA", "MAIZE", "BUMBA", "ABOLS", "VIOLE", "RADIO"];
+let vards;
+let k=0; //jauna vārda kārtas numurs
+let burti;
+let g;
+let rinda;
+
+function jaunsVards(){
+    vards=Vardi[k];
+    //console.log(vards);
+   
+}
+
+function BurtuIzveide(){
+    burti=vards.split("");
+    for (let i=0; i<vards.length; i++){
+        let m=burti[i];
+        let j=Math.floor(Math.random()*vards.length);
+        burti[i]=burti[j];
+        burti[j]=m;
+    }
+    return burti;
+}
+
+    
+//function BurtiPogaa(){
+   //for (let i=0; i<vards.length; i++){
+      
+      //document.getElementById("00").innnerHTML="2222";
+
+     // poga.addEventListener("click", ()=>{
+     // if(poga.innerText == "0"){
+      //  poga.innerText =burti[i];
+    //}})
+    
+//}
+
+function replaceButtonText(buttonId, text)
+{
+  if (document.getElementById)
+  {
+    var button=document.getElementById(buttonId);
+    if (button)
+    {
+      if (button.childNodes[0])
+      {
+        button.childNodes[0].nodeValue=text;
+      }
+      else if (button.value)
+      {
+        button.value=text;
+      }
+      else //if (button.innerHTML)
+      {
+        button.innerHTML=text;
+      }
+    }
+  }
+}   
+
+jaunsVards();
+BurtuIzveide();
+//BurtiPogaa();
+//console.log(g);
+console.log(burti);
+replaceButtonText('00','456560');
